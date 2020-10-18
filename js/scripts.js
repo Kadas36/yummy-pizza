63f2eb7bfd660515c524c03f90c0c2cc763509be
kadas36 <oduorthomas14@gmail.com>
2020-10-18 23:06:26 +0300

    Add hide and show to the form

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,13 @@ $(document).ready(function() {
     $("#formSubmit").submit(function(event) {
       event.preventDefault();
   
-      var inputtedSauce = $("[name='sauce']:checked").val();
-      var inputtedCheese = $("[name='cheese']:checked").val();
-      var inputtedVeggy = $("[name='veggy']:checked").val();
-      var inputtedBase = $("[name='base']:checked").val();
-      var inputtedSize = $("[name='grid']:checked").val();
+      let inputtedSauce = $("[name='sauce']:checked").val();
+      let inputtedCheese = $("[name='cheese']:checked").val();
+      let inputtedVeggy = $("[name='veggy']:checked").val();
+      let inputtedBase = $("[name='base']:checked").val();
+      let inputtedSize = $("[name='grid']:checked").val();
   
-      var newOrder = new Order(inputtedSauce, inputtedCheese, inputtedVeggy, inputtedBase, inputtedSize);
+      let newOrder = new Order(inputtedSauce, inputtedCheese, inputtedVeggy, inputtedBase, inputtedSize);
   
       $("ol#orders").append("<li><span class='orders'>" + newOrder.Sauce + "</span></li>");
   
