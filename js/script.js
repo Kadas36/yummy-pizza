5394f52c7a7ea192bcf74241b1023eba64d28d86
kadas36 <oduorthomas14@gmail.com>
2020-10-18 23:08:23 +0300

    Append li tag to ol

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("thumbnail").onclick = function() {
+    document.getElementById("myModa").style.display = "block";
+};
+
+document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementById("myModa").style.display = "none";
+}   
