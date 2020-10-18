9dcaee7e24eccfb4a3f6161a8348d29f0003cbb7
kadas36 <oduorthomas14@gmail.com>
2020-10-19 01:01:14 +0300

    Add prototype function to order

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,21 @@ function Order(sauce, cheese, veg, base, size) {
     this.Size = size;
 }
 
+Order.prototype.specs = function() {
+    return this.Size + " sized pizza with" + this.Sauce + " sauce, " + this.Cheese + " cheese, " + this.Veggy + ", on a " + this.Base + " base.";
+}
+
 
 $(document).ready(function() {
     
-
+    $(".thumbnail").click(function(event) {
+        event.preventDefault();
+        let pizza = $(this).text();
+    }); 
+    
     $("#formSubmit").submit(function(event) {
       event.preventDefault();
-  
+      
       let inputtedSauce = $("[name='sauce']:checked").val();
       let inputtedCheese = $("[name='cheese']:checked").val();
       let inputtedVeggy = $("[name='veggy']:checked").val();
@@ -21,10 +29,12 @@ $(document).ready(function() {
   
       let newOrder = new Order(inputtedSauce, inputtedCheese, inputtedVeggy, inputtedBase, inputtedSize);
   
-      $("ol#orders").append("<li><span class='orders'>" + newOrder.Sauce + "</span></li>");
+      $("ol#orders").append("<li><span class='orders'>" + newOrder.specs() + "</span></li>");
   
     //   $("input#new-first-name").val("");
     //   $("input#new-last-name").val("");
     });
+    
+    
 
 });
